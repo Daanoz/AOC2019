8afdda5a0fc4061777d9980dbb55c858b174a76a
Daan Sieben <developer@daansieben.net>
2019-12-01 09:22:31 +0100

    Day01

--- TS/01/index.ts
+++ TS/01/index.ts
@@ -2,7 +2,30 @@ import { Puzzle, Runner, BasePuzzle } from '../shared/';
 
 class PuzzleSolution extends BasePuzzle implements Puzzle {
     public run() {
-        console.log('HELLO WORLD 2345', this.getInput())
+        const input: number[] = this.getInputAsRows().map(r => parseInt(r, 10));
+        
+        const fuel = input
+            .map(n => this.calculateFuel(n))
+            .reduce((cur, n) => cur + n, 0);
+
+        console.log("Part A", fuel);
+
+
+        const totalFuel = input
+            .map(n => this.calculateRecusiveFuel(n))
+            .reduce((cur, n) => cur + n, 0);
+
+        console.log("Part B", totalFuel); 
+    }
+
+    private calculateRecusiveFuel(mass: number): number {
+        const fuel = this.calculateFuel(mass);
+        if (fuel <= 0) { return 0; } 
+        return fuel + this.calculateRecusiveFuel(fuel);
+    }
+
+    private calculateFuel(mass: number): number {
+        return Math.floor(mass / 3) - 2;
     }
 }
 
